1fdbf78c8a4c80d44aa2b93dd892e107a509f668
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-02-10 17:15:43 +0900

    Finish ch.4

--- src/EventPractice.tsx
+++ src/EventPractice.tsx
@@ -5,11 +5,13 @@ import { Component } from "react";
 
 export interface IEventPracticeState {
   message: string;
+  username: string;
 }
 
 class EventPractice extends Component<{}, IEventPracticeState> {
   public state = {
-    message: ""
+    message: "",
+    username: ""
   };
 
   public render() {
@@ -18,27 +20,43 @@ class EventPractice extends Component<{}, IEventPracticeState> {
         <h1>이벤트 연습</h1>
         <input
           type="text"
+          name="username"
+          placeholder="유저명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
           name="message"
           placeholder="아무거나 입력해보세요"
-          onChange={this.handleChange}
           value={this.state.message}
+          onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
         />
         <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
 
-  private handleChange = (e: any) => {
+  private handleChange = (e: React.SyntheticEvent) => {
+    const elem = e.target as HTMLInputElement;
     this.setState({
-      message: e.target.value
-    });
+      [elem.name]: elem.value
+    } as Pick<IEventPracticeState, keyof IEventPracticeState>);
   };
 
   private handleClick = () => {
-    alert(this.state.message);
+    alert(this.state.username + ": " + this.state.message);
     this.setState({
-      message: ""
+      message: "",
+      username: ""
     });
+  };
+
+  private handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      this.handleClick();
+    }
   };
 }
 
